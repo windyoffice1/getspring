5f1afbba6350fe17ad7b1a439166b42a68b01f15
windyoffice1 <kaikai888300@126.com>
2019-01-26 19:39:39 +0800

    commit merge

---
+++ src/test/java/org/getspring/test/v1/V1AllTests.java
@@ -0,0 +1,16 @@
+package org.getspring.test.v1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ApplicationContextTest.class, BeanFactoryTest.class, ResourceTest.class })
+public class V1AllTests {
+  
+	//2
+	//github.com
+	//eclipse
+	////新功能开发完121212
+	//test test
+}
